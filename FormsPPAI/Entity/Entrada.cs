9fafdeb1d5afeb85237cafdb3698ad22441037bc
82373 - Facundo - Rabinovich <80785557+82373FacundoRabinovich@users.noreply.github.com>
2021-07-30 16:52:55 -0300

    Commit arreglo

---
+++ FormsPPAI/Entity/Entrada.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashbord.Entity
+{
+    class Entrada
+    {
+        public DateTime FechaVencimiento { get; set; }
+        public DateTime HoraVenta { get; set; }
+        public float Monto { get; set; }
+        public int Numero { get; set; }
+    }
+}
