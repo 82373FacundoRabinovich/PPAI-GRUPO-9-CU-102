3771a9d6b4fa5d646a730eac70da7e53e8f2c89f
82373 - Facundo - Rabinovich <80785557+82373FacundoRabinovich@users.noreply.github.com>
2021-07-31 11:59:39 -0300

    Commit facu pre-Loop

--- FormsPPAI/DataAccessLayer/UsuarioAdapter.cs
+++ FormsPPAI/DataAccessLayer/UsuarioAdapter.cs
@@ -3,7 +3,7 @@ using System.Data;
 
 namespace Dashbord.DataAccessLayer {
 	public static class UsuarioAdapter {
-		public static DataTable ReadUsuario(string username, string password) => GeneralAdapter.Query("SELECT * FROM Usuario WHERE nombre = @username AND contraseña = @password", new Dictionary<string, string>() { { "@username", username }, { "@password", password } });
+		public static DataTable ReadUsuario(string username, string password) => GeneralAdapter.Query("SELECT e.* FROM Usuario u JOIN Empleado e ON u.id_empleado = e.dni WHERE u.nombre = @username AND u.contraseña = @password", new Dictionary<string, string>() { { "@username", username }, { "@password", password } });
 
 		public static DataTable ReadCargo(string username) => GeneralAdapter.Query("SELECT C.nombre FROM Usuario U JOIN Empleado E ON U.id_empleado = E.dni JOIN Cargo C ON E.id_cargo = C.id WHERE U.nombre = @username", new Dictionary<string, string>() { { "@username", username } });
 
